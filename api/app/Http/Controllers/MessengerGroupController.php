5db3d2d9f72ee512c8b854d577aa7a76bb1eab4d
johaines <insuteppukikku1@gmail.com>
2018-10-22 15:24:15 +0800

    update

--- api/app/Http/Controllers/MessengerGroupController.php
+++ api/app/Http/Controllers/MessengerGroupController.php
@@ -9,4 +9,22 @@ class MessengerGroupController extends ClassWorxController
     function __construct(){
       $this->model = new MessengerGroup();
     }
+
+
+    public function retrieve(Request $request){
+      $data = $request->all();
+      $this->retrieveDB($data);
+
+      $result = $this->response['data'];
+
+      if(sizeof($result) > 0){
+        $i = 0;
+        foreach ($result as $key) {
+          $result[$i]['account_details'] = $this->retrieveAccountDetails($result[$i]['account_id']);
+          $i++;
+        }
+        $this->response['data'] = $result;
+      }
+      return $this->response();
+    }
 }
