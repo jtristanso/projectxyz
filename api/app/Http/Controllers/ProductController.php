0d03e95abfcfb0bd98467194671303b669ea635e
johaines <insuteppukikku1@gmail.com>
2018-10-29 16:47:11 +0800

    update

--- api/app/Http/Controllers/ProductController.php
+++ api/app/Http/Controllers/ProductController.php
@@ -4,10 +4,34 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Product;
+use App\ProductPhoto;
 class ProductController extends ClassWorxController
 {
     function __construct(){
       $this->model = new Product();
       $this->notRequired = array('description');
+    }
+
+    public function retrieve(Request $request){
+      $data = $request->all();
+
+      $this->model = new Product();
+      $this->retrieveDB($data);
+      $products = $this->response['data'];
+
+      if(sizeof($products) > 0){
+        $i = 0;
+        foreach ($products as $key) {
+          $this->response['data'][$i]['photos'] = $this->getProductPhoto($products[$i]['id']);
+          $this->response['data'][$i]['account'] = $this->retrieveAccountDetails($products[$i]['account_id']);
+          $i++;
+        }
+      }
+      return $this->response();
+    }
+
+    public function getProductPhoto($productId){
+      $result = ProductPhoto::where('product_id', '=', $productId)->orderBy('status', 'ASC')->get();
+      return (sizeof($result) > 0) ? $result : null;
     }
 }
